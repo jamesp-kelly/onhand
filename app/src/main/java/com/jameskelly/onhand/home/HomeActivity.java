ef131d838db751def1ff14ae359c5dcff6ec3461
James Kelly <jk_jameskelly@outlook.com>
2016-04-14 09:50:43 -0400

    display camera image in preview

--- app/src/main/java/com/jameskelly/onhand/home/HomeActivity.java
+++ app/src/main/java/com/jameskelly/onhand/home/HomeActivity.java
@@ -1,33 +1,62 @@
 package com.jameskelly.onhand.home;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.jameskelly.onhand.R;
 import com.jameskelly.onhand.di.HomeModule;
 import com.jameskelly.onhand.di.OnHandApplication;
 import com.jameskelly.onhand.util.NotImplementedException;
+import java.io.File;
+import java.io.IOException;
 import javax.inject.Inject;
 
 public class HomeActivity extends AppCompatActivity implements HomeView {
 
+  private static final int TAKE_PICTURE_REQUEST_CODE = 1;
+
+  private Uri imageUri;
+
+  @Bind(R.id.image_preview) ImageView imagePreview;
+
   @Inject
   HomePresenter presenter;
+
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
 
-    ButterKnife.inject(this);
+    ButterKnife.bind(this);
 
     OnHandApplication.getInstance(this)
         .getAppComponent()
         .plus(new HomeModule(this))
         .inject(this);
 
-    //test dagger is working
-    //presenter.openCamera();
+  }
+
+  @OnClick(R.id.camera_button)
+  public void cameraClicked() {
+    presenter.openCamera();
+  }
+
+  @OnClick(R.id.gallery_button)
+  public void galleryClicked() {
+    presenter.openGallery();
   }
 
   @Override public void showPreviewImage(boolean confirmed) {
@@ -43,10 +72,35 @@ public class HomeActivity extends AppCompatActivity implements HomeView {
   }
 
   @Override public void showCamera() {
-    throw new NotImplementedException();
+    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    File photo = new File(Environment.getExternalStorageDirectory(), "ON_HAND_CAPTURE.jpg");
+    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+    imageUri = Uri.fromFile(photo);
+    startActivityForResult(intent, TAKE_PICTURE_REQUEST_CODE);
   }
 
   @Override public void showGallery() {
     throw new NotImplementedException();
+  }
+
+  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    switch (requestCode) {
+      case TAKE_PICTURE_REQUEST_CODE:
+        if (resultCode == Activity.RESULT_OK) {
+          Uri selectedImage = imageUri;
+          ContentResolver cr = getContentResolver();
+          cr.notifyChange(selectedImage, null);
+          Bitmap bitmap;
+          try {
+            bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, selectedImage);
+            imagePreview.setImageBitmap(bitmap);
+            Toast.makeText(this, selectedImage.toString(), Toast.LENGTH_SHORT).show();
+          } catch (IOException e) {
+            Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show();
+            Log.e(HomeActivity.class.getSimpleName(), e.toString());
+          }
+        }
+    }
   }
 }
--- app/src/main/java/com/jameskelly/onhand/lockscreen/LockScreenActivity.java
+++ app/src/main/java/com/jameskelly/onhand/lockscreen/LockScreenActivity.java
@@ -1,16 +1,12 @@
 package com.jameskelly.onhand.lockscreen;
 
-import android.app.WallpaperManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import butterknife.ButterKnife;
 import com.jameskelly.onhand.R;
 import com.jameskelly.onhand.di.LockScreenModule;
 import com.jameskelly.onhand.di.OnHandApplication;
-import java.io.IOException;
 
 public class LockScreenActivity extends AppCompatActivity implements LockScreenView {
 
@@ -18,7 +14,7 @@ public class LockScreenActivity extends AppCompatActivity implements LockScreenV
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home);
 
-    ButterKnife.inject(this);
+    ButterKnife.bind(this);
 
     OnHandApplication.getInstance(this)
         .getAppComponent()
@@ -35,31 +31,5 @@ public class LockScreenActivity extends AppCompatActivity implements LockScreenV
         WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
         WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     //
-
-
-    Button set = (Button) findViewById(R.id.set_button);
-    Button clear = (Button) findViewById(R.id.clear_button);
-
-    final WallpaperManager wallpaperManager = WallpaperManager.getInstance(this);
-
-    set.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        //try {
-        // wallpaperManager.setResource(R.raw.test_wallpaper);
-        //} catch (IOException e) {
-        //  e.printStackTrace();
-        //}
-      }
-    });
-
-    clear.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
-        try {
-          wallpaperManager.clear();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    });
   }
 }
