27131c6ded406fea6d47f7cdb61b6a8a9ad8a193
James Kelly <jk_jameskelly@outlook.com>
2016-04-19 11:59:47 -0400

    use exif interface to rotate correctly

--- app/src/main/java/com/jameskelly/onhand/base/BasePresenterImpl.java
+++ app/src/main/java/com/jameskelly/onhand/base/BasePresenterImpl.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.util.Log;
 import java.io.IOException;
@@ -16,17 +17,64 @@ public class BasePresenterImpl implements BasePresenter {
     try {
       bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, imageUri);
 
-      //force portrait todo: more general approach for handling image rotation
-      final int width = bitmap.getWidth();
-      final int height = bitmap.getHeight();
-      Matrix matrix = new Matrix();
-      matrix.postScale(1f, 1f);
-      matrix.postRotate(90);
-      bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+      ExifInterface exif = null;
+      try {
+        exif = new ExifInterface(imageUri.getPath());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+          ExifInterface.ORIENTATION_UNDEFINED);
+
+      bitmap = rotateBitmap(bitmap, orientation);
+
     } catch (IOException e) {
       Log.e(BasePresenterImpl.class.getSimpleName(), e.toString());
     }
 
     return bitmap;
+  }
+
+  private Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
+    Matrix matrix = new Matrix();
+    switch (orientation) {
+      case ExifInterface.ORIENTATION_NORMAL:
+        return bitmap;
+      case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+        matrix.setScale(-1, 1);
+        break;
+      case ExifInterface.ORIENTATION_ROTATE_180:
+        matrix.setRotate(180);
+        break;
+      case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+        matrix.setRotate(180);
+        matrix.postScale(-1, 1);
+        break;
+      case ExifInterface.ORIENTATION_TRANSPOSE:
+        matrix.setRotate(90);
+        matrix.postScale(-1, 1);
+        break;
+      case ExifInterface.ORIENTATION_ROTATE_90:
+        matrix.setRotate(90);
+        break;
+      case ExifInterface.ORIENTATION_TRANSVERSE:
+        matrix.setRotate(-90);
+        matrix.postScale(-1, 1);
+        break;
+      case ExifInterface.ORIENTATION_ROTATE_270:
+        matrix.setRotate(-90);
+        break;
+      default:
+        return bitmap;
+    }
+    try {
+      Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0,
+          bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+      bitmap.recycle();
+      return rotatedBitmap;
+    } catch (OutOfMemoryError e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 }
